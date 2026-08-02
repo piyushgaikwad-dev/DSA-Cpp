886fbdd0d04b6fc1dbda93b1c163c3bb22f381c1
piyushgaikwad-dev <piyush.gaikwad.dev@gmail.com>
2026-08-02 21:31:48 +0530

    Initial commit

---
+++ demo.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+
+void rotate(std::vector<std::vector<int>>& matrix) {
+    int n = matrix.size();
+    
+    // Step 1: Transpose the matrix
+    for (int i = 0; i < n; ++i) {
+        for (int j = i; j < n; ++j) {
+            std::swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+    
+    // Step 2: Reverse each row
+    for (int i = 0; i < n; ++i) {
+        std::reverse(matrix[i].begin(), matrix[i].end());
+    }
+}   
