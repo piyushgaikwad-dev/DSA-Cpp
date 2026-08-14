dbd4113074383156ca9fe98f4611f41a470c4208
piyushgaikwad-dev <piyush.gaikwad.dev@gmail.com>
2026-08-14 23:43:44 +0530

    Done

---
+++ Binary search/5.9.How many times Array has been Rotated.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[high]) {
+                // Minimum is in the right half
+                low = mid + 1;
+            } else {
+                // Minimum is at mid or in the left half
+                high = mid;
+            }
+        }
+
+        return nums[low];
+    }
+};
